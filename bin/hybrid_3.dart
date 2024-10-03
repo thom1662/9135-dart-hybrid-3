4284fdd5300714009e442e575e066bcb7bfab539
athom1662 <thom1662@algonquinlive.com>
2024-10-03 19:33:40 -0400

    cast json as the right kind of list

--- bin/hybrid_3.dart
+++ bin/hybrid_3.dart
@@ -1,9 +1,6 @@
 import 'dart:convert';
 
-
-
 void main(List<String> args) {
-
 
   String json = '''
 [
@@ -15,24 +12,31 @@ void main(List<String> args) {
 
 ''';
 
-var peopleList = jsonDecode(json);
-assert(peopleList is List, 'peopleList is not a List');
-  
-  //convert this json String into a List of Maps
+
+//jsondecode returns list <Dynamic>
+var profs = (jsonDecode(json) as List)
+  .map((item) => (item as Map<String, dynamic>)
+  .map((key, value) => MapEntry(key, value.toString()))) //change values to string
+  .toList();
+
+  //assert(profs is List<Map<String, String>>);
+  //var personOne = profs[0];
+  //print(personOne);
+
+
   //call Students constructor, passing the List
+  var folks = Students(profs);
+  folks.output();
   //call all methods once
 }
 
-//an array of key-val pairs,aka a list of maps
 
 class Students {
 
   late List <Map <String, String> > people;
 
 //constructor
-  Student(List <Map <String, String> > people){
-    this.people = people;
-  }
+  Students(this.people);
 
   //sort method, based on field name that is passed
   void sort(String field){
@@ -41,7 +45,9 @@ class Students {
 
 //method to loop and print each item in the list
   void output(){
-
+    for (var person in people){
+      print(person);
+    }
   }
 
 //method to add a new person to the list
